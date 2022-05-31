ac29b1b8d2b2331534817e5e9178faa43e0bc7bd
javier-cp <43758505+javier-cp6@users.noreply.github.com>
2022-05-30 19:34:57 -0500

    update

--- src/views/CartView.js
+++ src/views/CartView.js
@@ -48,32 +48,32 @@ export default function CartView() {
       <h1 className="mb-4 text-center">Your order</h1>
       <div className="row">
         <div className="d-flex flex-wrap text-center">
-          <div className="col-6 col-lg-3">Design</div>
-          <div className="col-6 col-lg-6">Product</div>
-          <div className="col-2 col-lg-3">Quantity</div>
+          <div className="col-6 col-md-3">Design</div>
+          <div className="col-6 col-md-6">Product</div>
+          <div className="col-2 col-md-3">Quantity</div>
         </div>
         <div>
           {cartProds.map((item, i) => (
             <div key={i} className="d-flex flex-wrap mb-2 border-bottom">
-              <div className="col-12 col-lg-3 text-center">
+              <div className="col-12 col-md-3 text-center">
                 <img src={item.prod_img} alt={item.prod_name} className="cart-img"/>
               </div>
-              <div className="d-flex flex-wrap col-12 col-lg-6 align-items-lg-center">
+              <div className="d-flex flex-wrap col-12 col-md-6 align-items-md-center">
                 <h5 className="col-12">{item.prod_name}</h5>
-                <p className="col-12">SKU ID: {item.prod_id}</p>
+                <div className="col-12">SKU ID: {item.prod_id}</div>
                 <div className="d-flex col-12">
-                  <p className="col-3">Size: {item.chosenSize}</p>
-                  <p className="col-3">Color: {item.chosenColor}</p>
+                  <div className="me-3">Size: {item.chosenSize}</div>
+                  <div className="me-3">Color: {item.chosenColor}</div>
+                  <button className="me-3">Remove</button>
                 </div>
-                <button>Remove</button>
               </div>
-              <div className="d-flex flex-wrap col-12 col-lg-3 text-center align-items-lg-center">
-                <input type="number" value={item.chosenQuantity} className="col-3 col-lg-3"></input>
-                <div></div>
-                <div className="col-3">items</div>
-                {/* <p className="col-6 col-lg-6">{item.chosenQuantity} items</p> */}
-                <div className="col-6 col-lg-6">Price: $ {item.prod_price}</div>
-                <div className="col-12 col-lg-12">Subtotal: $ {item.chosenQuantity * item.prod_price}</div>
+              <div className="d-flex flex-wrap col-12 col-md-3 text-center align-items-center">
+                <div className="d-flex col-12 col-md-12">
+                  <input type="number" value={item.chosenQuantity} className="col-3 col-md-3"></input>
+                  <div className="ms-2">items</div>
+                  <div className="col-6 col-md-6">Price: $ {item.prod_price}</div>
+                </div>
+                <div className="col-12 col-md-12">Subtotal: $ {item.chosenQuantity * item.prod_price}</div>
               </div>
             </div>
           ))}
